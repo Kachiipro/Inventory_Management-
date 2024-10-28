2e66e9f6468ca1071ec3f54dd3511e170c76bd74
Dikachi J <dikachimichael253@gmail.com>
2024-10-28 03:18:37 +0100

    corrected the database connection using oop

---
+++ dbconn.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+
+class Database:
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="52436809Dt@",
+            database="my_store"
+        )
+        self.cursor = self.conn.cursor()
+
+    def execute_q(self, query, params=None):
+        self.cursor.execute(query, params)
+        self.conn.commit()
+
+    def fetch_q(self, query, params=None):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
+
+    def close_conn(self):
+        self.cursor.close()
+        self.conn.close()
