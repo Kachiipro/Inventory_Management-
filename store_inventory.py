741def36af1f845205d1eb1a99faa2688660f3cc
Dikachi J <dikachimichael253@gmail.com>
2024-10-28 16:58:17 +0100

    added the update methods to store class

--- store_inventory.py
+++ store_inventory.py
@@ -11,3 +11,32 @@ class Store:
         self.db.execute_q(query, (products.name, products.category,
                           products.price, products.stock_quantity))
         print(f"Product '{products.name}' added successfully.")
+
+    def view_products(self):
+        query = "SELECT * FROM My_Inventory"
+        products = self.db.fetch_q(query)
+        if products:
+            for p in products:
+                print(p)
+        else:
+            print("no product In inventory")
+
+    def update_product(self, product_n, product_name):
+        query = "UPDATE My_Inventory SET product_name = %s WHERE product_name = %s "
+        self.db.execute_q(query, (product_name, product_n))
+        print("update succesfull")
+
+    def update_stock(self, product_id, quantity):
+        query = "UPDATE My_Inventory SET product_quantity = %s WHERE product_id = %s"
+        self.db.execute_q(query, (quantity, product_id))
+        print(f"Product ID {product_id} updated to quantity {quantity}.")
+
+    def update_category(self, product_n, category):
+        query = "UPDATE My_Inventory SET product_category = %s WHERE product_name = %s "
+        self.db.execute_q(query, (category, product_n))
+        print("update succesfull")
+
+    def update_price(self, product_n, price):
+        query = "UPDATE My_Inventory SET product_price = %s WHERE product_name = %s "
+        self.db.execute_q(query, (price, product_n))
+        print("update succesfull")
