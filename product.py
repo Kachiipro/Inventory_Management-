875448ba04a14c1d66a00575053e25fb415f6a5b
Dikachi J <dikachimichael253@gmail.com>
2024-10-28 03:23:41 +0100

    created product class with attributes

--- product.py
+++ product.py
@@ -1,9 +1,11 @@
+
+
 class Products:
-    def __init__(self, name, category, stock_quantity, price):
+    def __init__(self, name, category, price, stock_quantity):
         self.name = name
         self.category = category
         self.stock_quantity = stock_quantity
         self.price = price
 
     def __str__(self):
-        return f"{self.name} in {self.category} remaining {self.stock_quantity} sold at {self.price}"
+        return f"Product Name: {self.name} Category: {self.category} Stock: {self.stock_quantity} Price{self.price}"
